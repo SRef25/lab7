eb83fecf3d87a6f58eb0213ffdcffaa26db3cf73
Misael Nava <misaelnava812@gmail.com>
2023-10-30 16:42:03 -0600

    Activity 3 work in progress

--- src/main.c
+++ src/main.c
@@ -12,30 +12,32 @@
 #include <drivers/gpio.h>
 #include <sys/byteorder.h>
 
-#define RX_THREAD_STACK_SIZE 512
-#define RX_THREAD_PRIORITY 2
-#define STATE_POLL_THREAD_STACK_SIZE 512
-#define STATE_POLL_THREAD_PRIORITY 2
-#define LED_MSG_ID 0x10
-#define COUNTER_MSG_ID 0x12345
-#define SET_LED 1
-#define RESET_LED 0
-#define SLEEP_TIME K_MSEC(250)
-
 #define LED0_NODE DT_ALIAS(led0)
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#define LED1_NODE DT_ALIAS(led1)
+
+#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
 #define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#define PIN0	DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS0	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#define LED1	DT_GPIO_LABEL(LED1_NODE, gpios)
+#define PIN1	DT_GPIO_PIN(LED1_NODE, gpios)
+#define FLAGS1	DT_GPIO_FLAGS(LED1_NODE, gpios)
 #else
 /* A build error here means your board isn't set up to blink an LED. */
 #error "Unsupported board: led0 devicetree alias is not defined"
 #define LED0	""
-#define PIN	0
-#define FLAGS	0
+#define PIN0	0
+#define FLAGS0	0
+#define LED1	""
+#define PIN1	0
+#define FLAGS1	0
 #endif
 
 int tog = 0;
+int tog1 = 0;
+
+struct k_work work_queue;
+struct zcan_work rx_work;
 
 const struct device *can_dev;
 void activity0(const struct device *dev, const struct zcan_frame *msg, k_timeout_t timeout, can_tx_callback_t callback_isr, void *callback_arg);
@@ -44,17 +46,27 @@ void rx_callback_function(struct zcan_frame *frame, void *arg){
     //printk("I made it: %d", frame->data[0]);
     const struct device *ourLED;
     ourLED = device_get_binding(LED0);
-    gpio_pin_configure(ourLED, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+    gpio_pin_configure(ourLED, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
+
     if(tog == 0)
         tog = 1;
     else
         tog = 0;
     
-    gpio_pin_set(ourLED, PIN, tog);
+    gpio_pin_set(ourLED, PIN0, tog);
 }
 
-void activity1_callback_rx(struct zcan_frame *frame, void *arg){
+void activity3_callback_tx(struct zcan_frame *frame, void *arg){
+    const struct device *ourLED1;
+    ourLED1 = device_get_binding(LED1);
+    gpio_pin_configure(ourLED1, PIN1, GPIO_OUTPUT_ACTIVE | FLAGS1);
 
+    if(tog1 == 0)
+        tog1 = 1;
+    else
+        tog1 = 0;
+
+    gpio_pin_set(ourLED1, PIN1, tog1);
 }
 
 int main(){
@@ -92,10 +104,17 @@ int main(){
     int filter_id;
    
     can_dev =device_get_binding("CAN_1");
-    can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+    //Comment out loop back line for activity 2
+    can_set_mode(can_dev, CAN_NORMAL_MODE);
+    //can_set_mode(can_dev, CAN_LOOPBACK_MODE);
+    
     //activity 0
     //filter_id = can_attach_isr(can_dev, rx_callback_function, NULL, &my_filter);
-    filter_id = can_attach_isr(can_dev, rx_callback_function, NULL, &activity1_filter);
+    //filter_id = can_attach_isr(can_dev, rx_callback_function, NULL, &activity1_filter);
+
+
+    //activity3
+    filter_id = can_attach_workq(can_dev, &work_queue, &rx_work, rx_callback_function, NULL, &activity1_filter);
 
     if (filter_id <0){
         printk("unable to attach isr [%d]", filter_id);
@@ -116,7 +135,7 @@ int main(){
 
  void activity0(const struct device *dev, const struct zcan_frame *msg, k_timeout_t timeout, can_tx_callback_t callback_isr, void *callback_arg){
     int ret;
-    ret = can_send(dev, msg, K_MSEC(100), NULL,NULL);
+    ret = can_send(dev, msg, K_MSEC(100), activity3_callback_tx, NULL);
     if (ret != CAN_TX_OK){
         printk("Sending failed [%d]", ret);
     }
