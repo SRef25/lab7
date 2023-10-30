eb497cab39fe28b26e4f3f7df3483f8bbc84c503
Misael Nava <misaelnava812@gmail.com>
2023-10-30 17:37:55 -0600

    Updated Activity 3

--- src/main.c
+++ src/main.c
@@ -12,21 +12,21 @@
 #include <drivers/gpio.h>
 #include <sys/byteorder.h>
 
-#define LED0_NODE DT_ALIAS(led0)
+#define LED2_NODE DT_ALIAS(led2)
 #define LED1_NODE DT_ALIAS(led1)
 
 #if DT_NODE_HAS_STATUS(LED1_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN0	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS0	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#define LED2	DT_GPIO_LABEL(LED2_NODE, gpios)
+#define PIN2	DT_GPIO_PIN(LED2_NODE, gpios)
+#define FLAGS0	DT_GPIO_FLAGS(LED2_NODE, gpios)
 #define LED1	DT_GPIO_LABEL(LED1_NODE, gpios)
 #define PIN1	DT_GPIO_PIN(LED1_NODE, gpios)
 #define FLAGS1	DT_GPIO_FLAGS(LED1_NODE, gpios)
 #else
 /* A build error here means your board isn't set up to blink an LED. */
 #error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN0	0
+#define LED2	""
+#define PIN2	0
 #define FLAGS0	0
 #define LED1	""
 #define PIN1	0
@@ -45,15 +45,15 @@ void activity0(const struct device *dev, const struct zcan_frame *msg, k_timeout
 void rx_callback_function(struct zcan_frame *frame, void *arg){
     //printk("I made it: %d", frame->data[0]);
     const struct device *ourLED;
-    ourLED = device_get_binding(LED0);
-    gpio_pin_configure(ourLED, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
+    ourLED = device_get_binding(LED2);
+    gpio_pin_configure(ourLED, PIN2, GPIO_OUTPUT_ACTIVE | FLAGS0);
 
     if(tog == 0)
         tog = 1;
     else
         tog = 0;
     
-    gpio_pin_set(ourLED, PIN0, tog);
+    gpio_pin_set(ourLED, PIN2, tog);
 }
 
 void activity3_callback_tx(struct zcan_frame *frame, void *arg){
@@ -70,6 +70,7 @@ void activity3_callback_tx(struct zcan_frame *frame, void *arg){
 }
 
 int main(){
+    //activity 1
     struct zcan_frame frame = {
         .id_type = CAN_STANDARD_IDENTIFIER,
         .rtr = CAN_DATAFRAME,
@@ -84,6 +85,8 @@ int main(){
         .dlc =8,
         .data={8,7,6,5,5,6,7,8}
     };
+
+    //activity 0
     /**
     const struct zcan_filter my_filter = {
         .id_type = CAN_STANDARD_IDENTIFIER,
@@ -101,20 +104,38 @@ int main(){
         .id_mask = CAN_STD_ID_MASK
     };
 
+    //activity 3
+    struct zcan_frame loud = {
+        .id_type = CAN_STANDARD_IDENTIFIER,
+        .rtr = CAN_DATAFRAME,
+        .id = 0x120,
+        .dlc =8,
+        .data={1,2,3,4,5,6,7,8}
+    };
+    struct zcan_frame quiet = {
+        .id_type = CAN_STANDARD_IDENTIFIER,
+        .rtr = CAN_DATAFRAME,
+        .id = 0x123,
+        .dlc =8,
+        .data={1,2,3,4,5,6,7,8}
+    };
+
     int filter_id;
    
-    can_dev =device_get_binding("CAN_1");
+    can_dev = device_get_binding("CAN_1");
     //Comment out loop back line for activity 2
     can_set_mode(can_dev, CAN_NORMAL_MODE);
     //can_set_mode(can_dev, CAN_LOOPBACK_MODE);
     
     //activity 0
     //filter_id = can_attach_isr(can_dev, rx_callback_function, NULL, &my_filter);
+
+    //activity 1
     //filter_id = can_attach_isr(can_dev, rx_callback_function, NULL, &activity1_filter);
 
 
     //activity3
-    filter_id = can_attach_workq(can_dev, &work_queue, &rx_work, rx_callback_function, NULL, &activity1_filter);
+    filter_id = can_attach_workq(can_dev, &k_sys_work_q, &rx_work, rx_callback_function, NULL, &activity1_filter);
 
     if (filter_id <0){
         printk("unable to attach isr [%d]", filter_id);
@@ -126,16 +147,19 @@ int main(){
         //activity0(can_dev, &frame, K_MSEC(100),NULL,NULL);
         
         //activity 1
-        k_sleep(K_MSEC(1000));
-        activity0(can_dev, &frame, K_MSEC(100), NULL, NULL);
-        k_sleep(K_MSEC(1000));
-        activity0(can_dev, &frame2, K_MSEC(100), NULL, NULL);
+        //k_sleep(K_MSEC(1000));
+        //activity0(can_dev, &frame, K_MSEC(100), NULL, NULL);
+        //k_sleep(K_MSEC(1000));
+        //activity0(can_dev, &frame2, K_MSEC(100), NULL, NULL);
+    
+        //activity3
+        //activity0(can_dev, &loud, K_MSEC(100), NULL, NULL);
     }    
  }
 
  void activity0(const struct device *dev, const struct zcan_frame *msg, k_timeout_t timeout, can_tx_callback_t callback_isr, void *callback_arg){
     int ret;
-    ret = can_send(dev, msg, K_MSEC(100), activity3_callback_tx, NULL);
+    ret = can_send(dev, msg, timeout, activity3_callback_tx, NULL);
     if (ret != CAN_TX_OK){
         printk("Sending failed [%d]", ret);
     }
